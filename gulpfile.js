3bcb76d354cda0f4b0fa3bbda7806ea1ac0ef3b9
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-04 18:33:23 +0300

    create gulp css.min

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-});
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -14,4 +12,12 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', function () {
+    gulp.src('./*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css'));
 });
