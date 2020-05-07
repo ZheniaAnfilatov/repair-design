004175e766cb191b02c4a0267955eae37c944fb2
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-07 13:23:24 +0300

    feat: add gulp autopref

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 function bs() {
@@ -28,6 +29,9 @@ function def () {
 function serveSass() {
     return src("./sass.*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 }
