8326f9172d2687c1ffa7d61492a29969b25567e3
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-07 16:37:37 +0300

    feat: add futura book & demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ function bs() {
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
@@ -27,7 +28,7 @@ function def () {
 };
 
 function serveSass() {
-    return src("./sass.*.sass")
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
