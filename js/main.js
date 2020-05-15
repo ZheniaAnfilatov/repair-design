6d2eb2e3cc83135316f36651a0b2116a16c7ea0a
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-15 18:20:36 +0300

    feat: add jQuery and rewrite modal open and close

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const removeModal = () => {
-    modal.classList.remove('modal--visible')
-  }
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+
+
+//   const removeModal = () => {
+//     modal.classList.remove('modal--visible')
+//   }
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
+
+
+//   document.addEventListener('keypress', (event) => {
+//     if (event.key === 27);
+//     console.log('Esc')
+//     // removeModal();
+//   });
+
+//   document.addEventListener("click", (event) => {
+//     if (event.target == modal) {
+//     switchModal();
+//     }
+//   });
+
+
+// });
+
+
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  })
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
-  document.addEventListener('keydown', (event) => {
-    if (event.key === "Escape");
-    removeModal();
-  });
-
-  document.addEventListener("click", (event) => {
-    if (event.target == modal) {
-    switchModal();
-    }
-  });
-
 });
