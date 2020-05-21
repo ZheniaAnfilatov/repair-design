d5f1dd3c55a48d2e8204ae42c99a6b659ab0e8db
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-21 21:00:03 +0300

    feat: add php mailer

--- send.php
+++ send.php
@@ -18,14 +18,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'youremail@gmail.com';                     // SMTP username
-    $mail->Password   = 'pass';                               // SMTP password
+    $mail->Username   = 'zheniaanfilatov1@gmail.com';            // SMTP username
+    $mail->Password   = 'KREStrazh1812';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('youremail@gmail.com');
-    $mail->addAddress('client@yandex.ru');     // Add a recipient
+    $mail->setFrom('zheniaanfilatov1@gmail.com', 'Евгений');
+    $mail->addAddress('zheniaanfilatov@gmail.com');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
