714406400c851f83ddec8dc3b8ed237eabc3265b
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-21 22:03:36 +0300

    gmail

--- send.php
+++ send.php
@@ -18,14 +18,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = '';            // SMTP username
-    $mail->Password   = '';                               // SMTP password
+    $mail->Username   = 'zheniaanfilatov@gmail.com';            // SMTP username
+    $mail->Password   = 'KREStrazh1812';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('');
-    $mail->addAddress('');     // Add a recipient
+    $mail->setFrom('zheniaanfilatov@gmail.com');
+    $mail->addAddress('vikkisl@bk.ru');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
