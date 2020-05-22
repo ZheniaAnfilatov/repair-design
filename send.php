3ba19788563e8b7fed3622aae6e0d6d0a9805ad8
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-22 09:00:15 +0300

    add senf form for control and footer

--- send.php
+++ send.php
@@ -3,6 +3,8 @@
 $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
+
 
 // Load Composer's autoloader
 require 'phpmailer/PHPMailer.php';
@@ -31,7 +33,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Новая заявка с сайта';
-    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}, Вопрос пользователя: ${userQuestion}";
 
     $mail->send();
     header('Location: thanks.html');
