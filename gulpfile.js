fa844b5ed8e08cc610e3595b83175691c6f3f0c3
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-26 17:03:18 +0300

    maps api

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs() {
@@ -37,4 +41,57 @@ function serveSass() {
         .pipe(browserSync.stream());
 };
 
+function buildCSS(done) {
+    src('css/**/**.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('dist/css/'));
+    done();
+}
+
+function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js'])
+        .pipe(minify({ext:{
+                min:'.js'
+            }    
+        }))
+        .pipe(dest('dist/js/'));
+    src('js/**.min.js').pipe(dest('dist/js/'));
+    done();
+}
+
+function html(done) {
+    src('**.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist/'));
+    done();
+}
+
+function php(done) {
+    src('**.php')
+        .pipe(dest('dist/'));
+    src('phpmailer/**/**')
+        .pipe(dest('dist/phpmailer/'));
+    done();
+}
+
+function fonts(done) {
+    src('fonts/**/**')
+        .pipe(dest('dist/fonts/'));
+    done();
+}
+
+function imagemin(done) {
+    src('img/**/**')
+        .pipe(tinypng({key: 'xLCqfrD4qV6dYnzRDW1WqCxZTsBywsKy',}))
+        .pipe(dest('dist/img/'))
+    src('img/foto/**/**')
+        .pipe(tinypng({key: 'xLCqfrD4qV6dYnzRDW1WqCxZTsBywsKy',}))
+        .pipe(dest('dist/img/foto/'))
+    src('img/**/*.svg')
+        .pipe(dest('dist/img/'))
+    done();
+}
+
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin);
