7141a9216aec96e3538264def2c2003447b6e16b
Eugenii Anfilatov <63457807+ZheniaAnfilatov@users.noreply.github.com>
2020-05-27 15:28:31 +0300

    feat: add yandex metrica

--- js/main.js
+++ js/main.js
@@ -163,6 +163,7 @@ $(document).ready(function () {
           $(form)[0].reset();
           modal.removeClass('modal--visible');
           modalThanks.addClass('modal__thanks--visible');
+          ym(64399912, 'reachGoal', 'send'); return true;
         }
       });
     }
